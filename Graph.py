a741afd989df2268a333bd8794b08171e11080f8
Naoaki Sato <nsato@c4r3s7.42tokyo.jp>
2026-06-10 17:46:36 +0900

    docs: docstring
    docstringの追加。インスタンス作成時のエラー修正。

--- Graph.py
+++ Graph.py
@@ -5,9 +5,13 @@ from Connection import Connection
 
 
 class Graph:
-    """
-    """
     def __init__(self) -> None:
+        """
+            nb_drones: 全体のドローンの数
+            start_zone: スタート
+            end_zone: ゴール
+            zones: マップ全体のZoneの辞書
+        """
         self.nb_drones: int = 0
         self.start_zone: Zone | None = None
         self.end_zone: Zone | None = None
@@ -17,15 +21,23 @@ class Graph:
         self.zones[zone.name] = zone
 
     def add_connection(self, name1: str, name2: str, capacity: int) -> None:
+        """
+            Zone同士のConnectionを双方向のZoneに追加する。
+            Args:
+                name1: 1つ目のZone
+                name2: 2つ目のZone
+                capacity: Connectionのmax_link_capacity
+        """
         if name1 in self.zones and name2 in self.zones:
             zone1 = self.zones[name1]
             zone2 = self.zones[name2]
 
             two_way_reservations = defaultdict(int)
-            connect1 = Connection(zone2, capacity)
-            connect1.reservations = two_way_reservations
-            zone1.connections.append(Connection(connect1)
 
-            connect2 = Connection(zone1, capacity)
-            connect2.reservations = two_way_reservations
-            zone2.connections.append(Connection(connect2))
+            connection1 = Connection(zone2, capacity)
+            connection1.reservations = two_way_reservations
+            zone1.connections.append(connection1)
+
+            connection2 = Connection(zone1, capacity)
+            connection2.reservations = two_way_reservations
+            zone2.connections.append(connection2)
