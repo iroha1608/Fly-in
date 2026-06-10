429105f1d1d51aacdd37b2f93ac21c479e15919f
Naoaki Sato <nsato@c4r3s7.42tokyo.jp>
2026-06-10 17:49:03 +0900

    add: commit_path
    探索したパスを予約するメソッドを追加。

--- PathFinder.py
+++ PathFinder.py
@@ -72,7 +72,7 @@ class PathFinder:
                         ))
                 # "type=normal, priority": 1ターン消費
                 else:
-                    if target.can_enter(next_turn):
+                    if connection.can_enter(next_turn) and target.can_enter(next_turn):
                         heapq.heappush(queue, SearchState(
                             cost=state.cost + 1,
                             turn=state.turn + 1,
@@ -82,3 +82,37 @@ class PathFinder:
 
         # ゴールに到達しなかった場合
         return []
+
+    def commit_path(self, path_history: list[str]) -> None:
+        """
+            SearchStateから確定したpathを取り出し、
+            各Zone, Connectionに予約する。
+        """
+        # 初期位置: start_zone
+        current_location_name = self.graph.start_zone.name
+
+        for i, location_name in enumerate(path_history):
+            turn = i + 1
+
+            # Connection上の移動時
+            if "-" in location_name:
+                # restricted: start-normal_hub
+                name1, name2 = location_name.split("-")
+                # Zoneの持っているConnectionを1つづつチェック
+                for connection in self.graph.zones[name1].connections:
+                    if connection.target_zone.name == name2:
+                        connection.reserve(turn)
+                        break
+                current_location_name = location_name
+
+            # Zoneへの移動、または待機時
+            else:
+                self.graph.zones[location_name].reserve(turn)
+                # 移動時かつ前のターンがConnection上ではない時
+                if current_location_name != location_name and "-" not in current_location_name:
+                    # Connectionを走査し、移動したZoneがあれば予約する。
+                    for connection in self.graph.zones[current_location_name].connections:
+                        if connection.target_zone.name == location_name:
+                            connection.reserve(turn)
+                            break
+                current_location_name = location_name
